c1fdc15155b089ecee22999612742cdd5b2ac9a9
jayslen <jayslen134@gmail.com>
2025-05-28 22:55:15 -0400

    fix: update CORS origin to use environment variable for frontend URL

--- server/socket.js
+++ server/socket.js
@@ -3,7 +3,7 @@ import { Server } from 'socket.io'
 export function socket ({ lobbies, server }) {
   const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173'
+      origin: process.env.FRONTEND_URL ?? 'http://localhost:5173'
     }
   })
 
